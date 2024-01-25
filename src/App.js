ef29f6be5c07f4253017854391e0fb3faaf64ea6
Fausto Juarez <faustojuarezyelamos@gmail.com>
2024-01-25 18:47:15 -0300

    fourth exercise

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ let app = express();
 
 console.log("Hello world");
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
+
 absolutePath = __dirname + "/views/index.html";
 
 app.get("/", (req, res) => {
